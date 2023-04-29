2c704842790ea92307ffe0720760fab54fa17a38
yopa-ktc <yoparemy@gmail.com>
2023-04-29 18:16:03 +0200

    mis à jour de la classe arima_model

--- arima_model.py
+++ arima_model.py
@@ -1,14 +1,28 @@
 from statsmodels.tsa.arima.model import ARIMA
+import matplotlib.pyplot as plt
 
 class ArimaModel:
     
-    def __init__(self, df_train):
+    def __init__(self, df_log, df_train, df_test):
         self.df_train = df_train
+        self.df_test = df_test
+        self.df_log = df_log
         self.fitModel()
         
     def fitModel(self):
         model = ARIMA(self.df_train, order=(2,1,0))
-        #Entrainons notre modèle
-        model_fit = model.fit()
-        print(model_fit.summary())
+        model_fit = model.fit()        
         return model_fit
+    
+    def getResidus(self, model_fit):
+        residus = model_fit.resid[1:]
+        fig, ax = plt.subplots(1,2)
+        residus.plot(title='Residuals', ax=ax[0])
+        residus.plot(title='Density', kind='kde', ax=ax[1])
+        plt.show()
+        return residus
+    
+    def forecastTest(self, model_fit):
+        forecast_test = model_fit.forecast(len(self.df_test))
+        self.df_log['forecast_manual'] = [None]*len(self.df_train) + list(forecast_test)
+        plt.show()
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ df = pd.read_csv('website_data.csv')
 
 newModelPrepare = ModelPrepare(df)
 
-#________________________________________
 
 #newModelPrepare.visualisation()
 
@@ -17,67 +16,73 @@ df_log = newModelPrepare.normalisation()
 df_train, df_test = newModelPrepare.decoupage(df_log)
 
 """
-Checker la stationnarité
-Nous traçons la courbe et nous l'observons
-Si c'est plus difficile, nous traçons ACF ou PACF
+    Checker la stationnarité
+    Nous traçons la courbe et nous l'observons
+    Si c'est plus difficile, nous traçons ACF ou PACF
 """
 #acf_original, pacf_original = newModelPrepare.showACF(df_train)
 
+
 """
-VERIFICATION AVEC ADF
-Le test donne une valeur différente de 0 ce qui signifie
-que l'hypothèse de la non stationnarité est confirmée.
+    VERIFICATION AVEC ADF
+    Le test donne une valeur différente de 0 ce qui signifie
+    que l'hypothèse de la non stationnarité est confirmée.
 """
 newModelPrepare.showADF(df_train)
 
-"""
-Transformons notre série chronologique en série stationnaire
-Utilisons la fonction dropna()
-"""
-
 
 """
-   La p-value se rapproche encore plus de 0 ce qui
-signifie bien que notre modèle peut bien être stationnaire
-On peut laisser penser qu'à la 2e itération de différence,
-La stationnarité pourra encore s'améliorer
-
-On peut à présent déterminer le modèle ARIMA
-Les paramètres P et Q
-Le modèle (2,1,0)
+    Transformons notre série chronologique en série stationnaire
+    Utilisons la fonction dropna()
 """
-newArima = ArimaModel(df_train)
+#df_train_diff = df_train.diff().dropna()
+#df_train_diff.plot()
+
+#___Retraçons les ACF et PACF
+#acf_diff, pacf_diff = newModelPrepare.showACF(df_train_diff)
+#___Recalculons l'ADF 
+#adf_test_diff = newModelPrepare.showADF(df_train)
+
 
 """
-#Vérifions la qualité de notre modèle à l'aide des résidus
-#Si le modèle est bon, les résidus devraient ressembler à du bruit.
+    La p-value se rapproche encore plus de 0 ce qui
+    signifie bien que notre modèle peut bien être stationnaire
+    On peut laisser penser qu'à la 2e itération de différence,
+    La stationnarité pourra encore s'améliorer
 
-residus = model_fit.resid[1:]
-
-#fig, ax = plt.subplots(1,2)
-#residus.plot(title='Residuals', ax=ax[0])
-#residus.plot(title='Density', kind='kde', ax=ax[1])
-
-
-#On trace encore l'ACF et l'PACF des résidus
-#acf_resid = plot_acf(residus)
-#pacf_resid = plot_pacf(residus)
-#Le tracé montre bien que les résidus sont proches du bruit blanc
-#plt.show()
-
-#Nous sommes donc prêts à faire nos prédictions
-#Autrement dit, faisons le forecast
-
-forecast_test = model_fit.forecast(len(df_test))
-
-df_log['forecast_manual'] = [None]*len(df_train) + list(forecast_test)
+    On peut à présent déterminer le modèle ARIMA
+    Les paramètres P et Q
+    Le modèle (2,1,0)
+"""
+newArima = ArimaModel(df_log, df_train, df_test)
+model_fit = newArima.fitModel()
+#print(model_fit.summary())
 
 
+"""
+    #Vérifions la qualité de notre modèle à l'aide des résidus
+    #Si le modèle est bon, les résidus devraient ressembler à du bruit.
+"""
+#residus = newArima.getResidus(model_fit)
+
+
+"""
+    On trace encore l'ACF et l'PACF des résidus
+    Le tracé montre bien que les résidus sont proches du bruit blanc
+"""
+#acf_resid, pacf_resid = newModelPrepare.showACF(residus)
+
+"""
+    Nous sommes donc prêts à faire nos prédictions
+    Autrement dit, faisons le forecast
+"""
+newArima.forecastTest(model_fit)
+
+
+"""
 import pmdarima as pm
 auto_arima = pm.auto_arima(df_train, stepwise=False, seasonal=True)
-
 forecast_test_auto = auto_arima.predict(n_periods=len(df_test))
-
 df_log['forecast_auto'] = [None]*len(df_train) + list(forecast_test_auto)
 df_log.plot()
 plt.show()
